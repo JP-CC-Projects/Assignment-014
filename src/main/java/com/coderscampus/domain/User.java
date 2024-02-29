d7fa1f87b95db8e2d027533f71da56e852b8061e
JP-CC-Projects <joeparkcc@gmail.com>
2024-02-29 10:44:54 -0600

    first commit

---
+++ src/main/java/com/coderscampus/domain/User.java
@@ -0,0 +1,20 @@
+package com.coderscampus.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
